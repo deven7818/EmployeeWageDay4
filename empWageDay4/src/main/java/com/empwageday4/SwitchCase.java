ac5db3a99a53b4ab954c6d6c1c43bc848b1c1f8b
deven7818 <deven7818@gmail.com>
2022-02-16 23:29:39 +0530

    Emp wage with switch case Uc4

---
+++ empWageDay4/src/main/java/com/empwageday4/SwitchCase.java
@@ -0,0 +1,37 @@
+package com.empwageday4;
+
+import java.util.Random;
+
+public class SwitchCase extends PartTime {
+
+	public static final int isFullTime = 1, isPartTime = 2;
+
+	public static int SwitchCase() {
+		Random random = new Random();
+		int num = random.nextInt(3);
+		return num;
+	}
+
+	public static void main(String[] args) {
+
+		int check = SwitchCase();
+
+		switch (check) {
+
+		case isFullTime: // full time wage
+
+			System.out.println("Employee Full day Wager = " + wager());
+			break;
+
+		case isPartTime: // part time wage
+
+			System.out.println("Employee Part time Wager = " + partTimeWage());
+			break;
+			
+		default:
+			System.out.println("Employee is Absent"); // Absent
+		}
+
+	}
+
+}
